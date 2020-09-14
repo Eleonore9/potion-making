823839572cf266774e86abc3cf32dea07f470e0a
Éléonore Mayola <eleonore.mayola@gmail.com>
2020-09-14 12:54:51 +0200

    Upgrade deps

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
 (defproject program-languages-resources "1.0.0-SNAPSHOT"
-  :description "Demo Clojure web app"
+  :description "Clojure mini API"
   :url "http://potion-making.herokuapp.com/"
   :license {:name "Eclipse Public License v1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
-                 [ring/ring-jetty-adapter "1.4.0"]
-                 [environ "1.0.0"]
-                 [camel-snake-kebab "0.2.4"]
-                 [org.clojure/data.json "0.2.6"]
-                 [org.clojure/data.csv "0.1.3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.2"]
+                 [ring/ring-jetty-adapter "1.8.1"]
+                 [environ "1.2.0"]
+                 [camel-snake-kebab "0.4.1"]
+                 [org.clojure/data.json "1.0.0"]
+                 [org.clojure/data.csv "1.0.0"]]
   :min-lein-version "2.0.0"
-  :plugins [[environ/environ.lein "0.3.1"]]
+  :plugins [[environ/environ.lein "1.2.0"]]
   :hooks [environ.leiningen.hooks]
   :uberjar-name "program-languages-resources-standalone.jar"
   :profiles {:production {:env {:production true}}})
